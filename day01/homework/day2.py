65f049b5abd367890608bb96540f26aaec01786d
nikaarjevanidze <nikolozarjevanidze2013@gmail.com>
2024-07-10 18:46:55 +0400

    goa

---
+++ day01/homework/day2.py
@@ -0,0 +1,10 @@
+name="nika"
+surname="arjevanidze"
+age=11
+print ( " i am " +  name + " " + surname + " " + str(age)  +  " years old ")
+
+
+
+
+
+
---
+++ day03/arpa.py
@@ -0,0 +1,4 @@
+name="nika"
+surname="arjevanidze"
+strage="11"
+print( " i am " + name + " " + surname + " " + strage + " years old ")
